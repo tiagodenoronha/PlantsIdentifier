a0974b0aa42b68ddf0dae2c8b6615df30203f882
Tiago Noronha <27889458+tiagodenoronha@users.noreply.github.com>
2018-10-14 13:10:42 +0100

    First passing test!

--- tests/PlantsIdentifierAPI.UnitTests/UnitTest1.cs
+++ tests/PlantsIdentifierAPI.UnitTests/UnitTest1.cs
@@ -1,23 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PlantsIdentifierAPI.Controllers;
+using PlantsIdentifierAPI.Interfaces;
 using PlantsIdentifierAPI.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace PlantsIdentifierAPI.UnitTests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Plants_Returns()
-        {
-            var mockRepo = new Mock<PlantsContext>();
-           // mockRepo.Setup(repo => repo.Plant).ReturnsAsync(FillPlants());
-            var controller = new PlantsController(mockRepo.Object);
+	public class UnitTest1
+	{
+		[Fact]
+		public void Plants_ReturnsEmpty()
+		{
+			var mockRepo = new Mock<IPlantsServices>();
+		   // mockRepo.Setup(repo => repo.Plant).ReturnsAsync(FillPlants());
+			var controller = new PlantsController(mockRepo.Object);
 
-            var result = controller.Get();
+			var result = controller.Get();
+			Assert.IsType<ActionResult<IEnumerable<Plant>>>(result);
+			Assert.Null(result.Value);
 
+		}
+		[Fact]
+		public void Plants_ReturnsOk()
+		{
+			var numberOfPlants = 5;
+			var mockRepo = new Mock<IPlantsServices>();
+			mockRepo.Setup(repo => repo.GetAll()).Returns(Enumerable.Repeat(Mock.Of<Plant>(), numberOfPlants));
+			// mockRepo.Setup(repo => repo.Plant).ReturnsAsync(FillPlants());
+			var controller = new PlantsController(mockRepo.Object);
 
-        }
-    }
+			var result = controller.Get();
+			Assert.NotEmpty(result.Value);
+			
+
+		}
+	}
 }
