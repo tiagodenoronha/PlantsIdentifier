45001d34de692a83f69bccd1554b4470d15989fd
Tiago Noronha <27889458+tiagodenoronha@users.noreply.github.com>
2018-10-15 18:55:37 +0100

    Holding off services until we find a way to properly test DBContext

--- tests/PlantsIdentifierAPI.UnitTests/Services/PlantsServicesTests.cs
+++ tests/PlantsIdentifierAPI.UnitTests/Services/PlantsServicesTests.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Moq;
+using PlantsIdentifierAPI.Interfaces;
+using PlantsIdentifierAPI.Models;
+using PlantsIdentifierAPI.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace PlantsIdentifierAPI.UnitTests.Services
 {
 	public class PlantsServicesTests
 	{
+		//[Fact]
+		//public void Plants_GetAll_ReturnsOk()
+		//{
+		//	//Arrange
+		//	var mockContext = new Mock<PlantsContext>();
+		//	var service = new PlantsServices(mockContext.Object);
+
+		//	//Act
+		//	var result = service.GetAll();
+
+		//	//Assert
+		//	Assert.Empty(result);
+		//}
+
+		//[Fact]
+		//public void Plants_GetAll_ReturnsNull()
+		//{
+
+		//}
+
 	}
 }
