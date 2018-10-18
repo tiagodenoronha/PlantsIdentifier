d0a68dff1f1b0a9628b5413c14140c605a3d26d2
Tiago Noronha <27889458+tiagodenoronha@users.noreply.github.com>
2018-10-18 21:59:25 +0100

    Cleanedup references. Returned to IEnumerable on PlantsServices. Start of final test methods.

--- tests/PlantsIdentifierAPI.UnitTests/Services/PlantsServicesTests.cs
+++ tests/PlantsIdentifierAPI.UnitTests/Services/PlantsServicesTests.cs
@@ -6,47 +6,93 @@ using PlantsIdentifierAPI.Models;
 using PlantsIdentifierAPI.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
 namespace PlantsIdentifierAPI.UnitTests.Services
 {
-	public class PlantsServicesTests
-	{
+    public class PlantsServicesTests
+    {
 
-		public DbContextOptions<PlantsContext> DefaultOptions { get; } = new DbContextOptionsBuilder<PlantsContext>().Options;
+        public DbContextOptions<PlantsContext> DefaultOptions { get; } = new DbContextOptionsBuilder<PlantsContext>().Options;
 
-		[Fact]
-		public void Plants_GetAll_ReturnsOneElement()
-		{
-			//Arrange
-			var dbContextMock = new DbContextMock<PlantsContext>(DefaultOptions);
-			var plantsInitialDBSet = dbContextMock.CreateDbSetMock(x => x.Plant, new[] { new Plant { } });
-			var service = new PlantsServices(dbContextMock.Object);
+        [Fact]
+        public void Plants_GetAll_ReturnsOneElement()
+        {
+            //Arrange
+            var dbContextMock = new DbContextMock<PlantsContext>(DefaultOptions);
+            var plantsInitialDBSet = dbContextMock.CreateDbSetMock(x => x.Plant, new[] { new Plant { } });
+            var service = new PlantsServices(dbContextMock.Object);
 
-			//Act
-			var result = service.GetAll();
+            //Act
+            var result = service.GetAll();
 
-			//Assert
-			Assert.NotEmpty(result);
-		}
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.True(result.Any());
+        }
 
-		[Fact]
-		public void Plants_GetAll_ReturnsEmpty()
-		{
+        [Fact]
+        public void Plants_GetAll_ReturnsEmpty()
+        {
 
-			//Arrange
-			var dbContextMock = new DbContextMock<PlantsContext>(DefaultOptions);
-			var plantsInitialDBSet = dbContextMock.CreateDbSetMock(x => x.Plant);
-			var service = new PlantsServices(dbContextMock.Object);
+            //Arrange
+            var dbContextMock = new DbContextMock<PlantsContext>(DefaultOptions);
+            var plantsInitialDBSet = dbContextMock.CreateDbSetMock(x => x.Plant);
+            var service = new PlantsServices(dbContextMock.Object);
 
-			//Act
-			var result = service.GetAll();
+            //Act
+            var result = service.GetAll();
 
-			//Assert
-			Assert.NotNull(result);
-			Assert.Empty(result);
-		}
+            //Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
 
-	}
+        [Fact]
+        public void Plants_GetPlantByID_ReturnsNull()
+        {
+
+            //Arrange
+            //var dbContextMock = new DbContextMock<PlantsContext>(DefaultOptions);
+            //var plantsInitialDBSet = dbContextMock.CreateDbSetMock(x => x.Plant);
+            //var service = new PlantsServices(dbContextMock.Object);
+
+            ////Act
+            //var result = service.GetAll();
+
+            ////Assert
+            //Assert.NotNull(result);
+            //Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Plants_GetPlantByID_ReturnsOk()
+        {
+
+        }
+
+        [Fact]
+        public void Plants_GetPlantByCommonName_ReturnsNull()
+        {
+        }
+
+        [Fact]
+        public void Plants_GetPlantByCommonName_ReturnsOk()
+        {
+        }
+
+        [Fact]
+        public void Plants_SavePlant_ReturnsOk()
+        { }
+
+        [Fact]
+        public void Plants_SavePlant_ReturnsNull()
+        { }
+
+
+
+    }
 }
