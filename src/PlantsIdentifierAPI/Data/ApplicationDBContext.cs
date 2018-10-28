c38342b34acd745ef7bfab469c0147d441f473ec
Tiago Noronha <27889458+tiagodenoronha@users.noreply.github.com>
2018-10-28 17:36:30 +0000

    Code Factor.

--- src/PlantsIdentifierAPI/Data/ApplicationDBContext.cs
+++ src/PlantsIdentifierAPI/Data/ApplicationDBContext.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PlantsIdentifierAPI.Models;
@@ -6,16 +5,16 @@ using PlantsIdentifierAPI.Models;
 namespace PlantsIdentifierAPI.Data
 {
 	public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
-    {
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
-                : base(options)
-        {
-        }
+	{
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+				: base(options)
+		{
+		}
 
-        public virtual DbSet<Plant> Plant { get; set; }
-    }
+		public virtual DbSet<Plant> Plant { get; set; }
+	}
 }
